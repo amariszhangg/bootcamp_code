1f59a458f972a843f09eb3bddb6329751854440a
amariszhangg <115908439+amariszhangg@users.noreply.github.com>
2022-10-16 15:19:35 +1100

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,13 @@
+"""
+Programming Task #2
+Create a game where the user must correctly guess a randomly generated integer between 1 and 100.
+
+The user should be notified whether their guess was “lower” or “higher” than the target number.
+
+Note that you will need to use the random library’s randint function.
+
+Remember:
+- Use functions to group code
+- Try to have functions that only do one “thing” (e.g. print a message, validate user input, check what message to display)
+- Check for invalid inputs, notifying the user and re-prompting for a valid input
+"""
