9dc70671035b613c11aacfc085d57519bba02d7d
amariszhangg <115908439+amariszhangg@users.noreply.github.com>
2022-11-30 15:22:59 +1100

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,9 +1,7 @@
 """
 Programming Task #2
 Create a game where the user must correctly guess a randomly generated integer between 1 and 100.
-
 The user should be notified whether their guess was “lower” or “higher” than the target number.
-
 Note that you will need to use the random library’s randint function.
 
 Remember:
@@ -11,3 +9,32 @@ Remember:
 - Try to have functions that only do one “thing” (e.g. print a message, validate user input, check what message to display)
 - Check for invalid inputs, notifying the user and re-prompting for a valid input
 """
+import random
+
+def int_randomiser():  # generates the target number using the random module
+    num = random.randint(1,100)
+    return num
+
+def int_validator(guess: str):  # either v
+    try:  # if the guess can be converted to integer type
+        if 1 <= int(guess) <= 100:  # if the guess is within the valid range
+            return 'valid'
+        elif int(guess) < 1 or int(guess) > 100:  # if the guess is not within the valid range
+            return 'invalid'
+    except TypeError as e:  # if the guess cannot be converted to integer type
+        return 'invalid'
+
+def int_guesser():
+    target = int_randomiser()
+    guess = input('Guess a number between 1 to 100.')
+
+    while int_validator(guess) == 'invalid':  # if the guess is invalid, the user will have to re-input it
+        guess = input('Guess a VALID number between 1 to 100.')
+    # breaks out of the while loop when 'valid' is returned
+
+    if int(guess) < target:
+        return "Your guess was lower than the target number."
+    elif int(guess) > target:
+        return "Your guess was higher than the target number."
+    elif int(guess) == target:
+        return "Your guess was correct."
